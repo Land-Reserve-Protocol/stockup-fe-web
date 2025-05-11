f72955b612793403b39cb3c502b15d2a92ab9a41
Constantine <Okonemmanuelmma@gmail.com>
2025-05-11 17:43:52 +0100

    added button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,17 @@
 import type { ButtonProps } from "../../common/types/componentTypes";
+import Loader from "../loader";
 
 function Button({ style, onClick, text, loading, isDisabled }: ButtonProps) {
   return (
     <button
-      className={`${style} bg-[#f1f1f1] rounded-[8px]`}
+      className={`${style} bg-[#f1f1f1] rounded-[8px] 
+      text-center flex justify-center items-center text-[#1e1e1e] 
+      font-[700] text-[16px] leading-[24px] cursor-pointer hover:bg-[#e0e0e0] 
+      transition-all duration-200 ease-in-out`}
       disabled={loading || isDisabled}
       onClick={() => onClick()}
     >
-      {!loading ? text : <div className="loader"></div>}
+      {!loading ? text : <Loader size={24} color="#1E1E1E" />}
     </button>
   );
 }
--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,4 +1,10 @@
-export default function Loader({ size = 24, color = "text-blue-500" }) {
+export default function Loader({
+  size = 24,
+  color = "#1e1e1e",
+}: {
+  size?: number;
+  color?: string;
+}) {
   return (
     <div
       className={`animate-spin rounded-full border-4 border-t-transparent ${color}`}
