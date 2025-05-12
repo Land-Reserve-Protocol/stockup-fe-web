c8c828e4e2c1d73c9d761b21f0495302a356f992
Constantine <Okonemmanuelmma@gmail.com>
2025-05-12 22:39:14 +0100

    create a wallet providers list

---
+++ src/common/WalletProvider/providers.ts
@@ -0,0 +1,49 @@
+// Ethereum Wallets
+const ethereumWallets = [
+  { name: "isMetaMask", flag: "isMetaMask" },
+  { name: "isCoinbaseWallet", flag: "isCoinbaseWallet" },
+  { name: "isTrustWallet", flag: "isTrustWallet" },
+  { name: "isBinanceWallet", flag: "isBinanceWallet" },
+  { name: "isTokenPocket", flag: "isTokenPocket" },
+  { name: "isSafePal", flag: "isSafePal" },
+  { name: "isMathWallet", flag: "isMathWallet" },
+  { name: "isNiftyWallet", flag: "isNiftyWallet" },
+  { name: "isDapperWallet", flag: "isDapperWallet" },
+  { name: "isBraveWallet", flag: "isBraveWallet" },
+];
+
+// Solana Wallets
+const solanaWallets = [
+  { name: "isPhantom", flag: "isPhantom" },
+  { name: "isSolflare", flag: "isSolflare" },
+  { name: "isBackpack", flag: "isBackpack" },
+  { name: "isGlow", flag: "isGlow" },
+  { name: "isSlope", flag: "isSlope" },
+  { name: "isCoin98", flag: "isCoin98" },
+  { name: "isBitKeep", flag: "isBitKeep" },
+  { name: "isMathWallet", flag: "isMathWallet" },
+  { name: "isTokenPocket", flag: "isTokenPocket" },
+  { name: "isBraveWallet", flag: "isBraveWallet" },
+  { name: "isTorus", flag: "isTorus" },
+  { name: "isExodus", flag: "isExodus" },
+  { name: "isLedger", flag: "isLedger" },
+  { name: "isSollet", flag: "isSollet" },
+  { name: "isSolong", flag: "isSolong" },
+  { name: "isSafePal", flag: "isSafePal" },
+  { name: "isTrust", flag: "isTrust" },
+  { name: "isZelcore", flag: "isZelcore" },
+  { name: "isCoinbaseWallet", flag: "isCoinbaseWallet" },
+];
+
+// Cardano Wallets
+const cardanoWallets = [
+  { name: "isYoroi", flag: "isYoroi" },
+  { name: "isDaedalus", flag: "isDaedalus" },
+  { name: "isNami", flag: "isNami" },
+  { name: "isTyphon", flag: "isTyphon" },
+  { name: "isLace", flag: "isLace" },
+  { name: "isGeroWallet", flag: "isGeroWallet" },
+  { name: "isFlint", flag: "isFlint" },
+  { name: "isEternl", flag: "isEternl" },
+  { name: "isSeiza", flag: "isSeiza" },
+];
