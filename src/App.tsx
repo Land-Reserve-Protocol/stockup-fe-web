b826cc01dfebd8de3b36dea6be843a195ca45f19
Constantine <Okonemmanuelmma@gmail.com>
2025-06-09 21:18:18 +0100

    Added navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Provider } from "react-redux";
 import "./App.css";
 import { store } from "./Redux/store";
+import Navbar from "./components/Navbar/navbar";
 // import WalletLists from "./components/walletListComponent/walletLists";
 
 function App() {
   return (
     <Provider store={store}>
-      <div></div>
+      <Navbar />
     </Provider>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    strictPort: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
