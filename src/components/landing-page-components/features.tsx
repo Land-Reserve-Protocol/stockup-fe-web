7ecd496d24857e89559b7b6679bbe2f25808af5a
Constantine <Okonemmanuelmma@gmail.com>
2025-06-10 03:40:59 +0100

    little changes

--- src/components/landing-page-components/features.tsx
+++ src/components/landing-page-components/features.tsx
@@ -4,8 +4,10 @@ import { MdPeopleAlt } from "react-icons/md";
 
 function CoreFeatures() {
   return (
-    <div className="h-[100vh] bg-[#233246] flex flex-col justify-between">
-      <div className="flex flex-col items-center justify-center gap-[20px] h-[60%]">
+    <div className="h-[100vh] bg-[#00000090] flex flex-col justify-between relative">
+      <div className="absolute top-0 left-0 w-full h-full bg-[linear-gradient(to_top,_#00000090_10%,_#00000090_20%)] z-0 "></div>
+
+      <div className="flex flex-col items-center justify-center gap-[20px] h-[60%] z-99">
         <p className="text-center text-[34px] font-bold text-[#C8CBCC]">
           Core Features
         </p>
@@ -13,7 +15,7 @@ function CoreFeatures() {
           Revolutionary DeFi infrastructure backed by real-world assets
         </p>
       </div>
-      <div className="w-[1440px] h-[80%] flex  content-center mx-auto justify-between">
+      <div className="w-[1440px] h-[80%] flex  content-center mx-auto justify-between z-99">
         <div className="w-[400px] h-[350px] bg-[#1E2F34] flex flex-col content-center pl-[20px] pr-[30px] pt-[50px] gap-[30px] rounded-[8px] ring ring-2 ring-[#25433D] ring-offset-2 ring-offset-[#1E2F34]">
           <div className="bg-[#5C532D] h-[54px] w-[54px] flex justify-center items-center rounded-lg">
             <FaCoins className="text-[#C3A437] text-[24px]" />
