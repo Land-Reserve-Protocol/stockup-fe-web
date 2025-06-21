2a0ab7b8833256abc4f211cbd73541d7d296519a
Constantine <Okonemmanuelmma@gmail.com>
2025-06-21 20:04:44 +0100

    finally finished this stuff

--- src/components/landing-page-components/how-it-works.tsx
+++ src/components/landing-page-components/how-it-works.tsx
@@ -42,17 +42,18 @@ function HowItWorks() {
   const [activeStep, setActiveStep] = useState(0);
 
   return (
-    <div className="relative text-white bg-[#00020b] py-20 px-4 sm:px-6 lg:px-16 relative">
-      <div className="max-w-7xl mx-auto flex flex-col items-center justify-center mb-10">
-        <h2 className="text-2xl sm:text-3xl lg:text-[34px] font-semibold text-[#C8CBCC] mb-4">
-          Land Tokenization Process
-        </h2>
-        <p className=" text-center pt-5 text-base sm:text-lg lg:text-[24px] font-medium text-[#7A8187] leading-relaxed max-w-xl">
-          See how real-world land assets are transformed into on-chain value
-          through our innovative protocol.
-        </p>
-      </div>
-      {/* <div className="hidden lg:flex lg:items-center lg:justify-center lg:m-20 bg-[#1F2937] lg:p-4 lg:rounded-xl lg:shadow-xl lg:shadow-black/70 lg:space-x-4">
+    <div className="slider-container">
+      <div className="relative text-white bg-[#00020b] py-20 px-4 sm:px-6 lg:px-16 relative slider-matrix">
+        <div className="max-w-7xl mx-auto flex flex-col items-center justify-center mb-10 mt-10">
+          <h2 className="text-2xl sm:text-3xl lg:text-[34px] font-semibold text-[#C8CBCC] mb-4">
+            Land Tokenization Process
+          </h2>
+          <p className=" text-center pt-5 text-base sm:text-lg lg:text-[24px] font-medium text-[#7A8187] leading-relaxed max-w-xl">
+            See how real-world land assets are transformed into on-chain value
+            through our innovative protocol.
+          </p>
+        </div>
+        {/* <div className="hidden lg:flex lg:items-center lg:justify-center lg:m-20 bg-[#1F2937] lg:p-4 lg:rounded-xl lg:shadow-xl lg:shadow-black/70 lg:space-x-4">
         {steps.map((step, index) => (
           <Button
             key={step.title}
@@ -76,14 +77,14 @@ function HowItWorks() {
         ))}
       </div> */}
 
-      <div className="container max-w-7xl py-16 right-[200px] m-auto slider-container ">
-        <div className="slider-matrix">
-          <div className="flex justify-between items-center content-center  slider-elements">
-            {steps.map(({ title, content }, index) => (
-              <div
-                onClick={() => setIsActive(title)}
-                key={title}
-                className={`
+        <div className="container max-w-7xl py-5 m-auto">
+          <div className="">
+            <div className="flex justify-between items-center content-center  slider-elements">
+              {steps.map(({ title, content }, index) => (
+                <div
+                  onClick={() => setIsActive(title)}
+                  key={title}
+                  className={`
           ${
             active === title
               ? "lg:bg-[#1F2937] bg-[transparent]"
@@ -92,19 +93,22 @@ function HowItWorks() {
           text-white  p-8 min-h-[300px] shadow-xl shadow-black/70 rounded-2xl 
           transition-all duration-300 ease-in-out
          cursor-pointer max-w-4xl m-auto w-full sm:mx-[50px]`}
-              >
-                <div
-                  className="flex flex-col justify-center items-start
-          text-white "
                 >
-                  <h2 className="text-2xl font-bold mb-2">{title}</h2>
-                  <p className="text-md leading-relaxed w-[350px]">{content}</p>
+                  <div
+                    className="flex flex-col justify-center items-start
+          text-white "
+                  >
+                    <h2 className="text-2xl font-bold mb-2">{title}</h2>
+                    <p className="text-md leading-relaxed w-[350px]">
+                      {content}
+                    </p>
+                  </div>
+                  <div>
+                    <img src={images[index]} className="h-[200px] w-[200px]" />
+                  </div>
                 </div>
-                <div>
-                  <img src={images[index]} className="h-[200px] w-[200px]" />
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
